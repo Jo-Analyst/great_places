f82f6bd1ab757dfefc7f08a4143f4d0210e45401
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-03-17 18:12:14 -0300

    338. Componente Lista de Lugares

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
   const PlacesListScreen({super.key});
@@ -18,9 +20,21 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: CircularProgressIndicator(
-          color: Theme.of(context).colorScheme.secondary,
+      body: Consumer<GreatPlaces>(
+        builder: (ctx, greatPlaces, ch) => greatPlaces.itemsCount == 0
+            ? ch!
+            : ListView.builder(
+                itemCount: greatPlaces.itemsCount,
+                itemBuilder: (ctx, index) => ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage:
+                        FileImage(greatPlaces.itemByIndex(index).image),
+                  ),
+                  title: Text(greatPlaces.itemByIndex(index).title),
+                ),
+              ),
+        child: const Center(
+          child: Text("Nenhum local cadastrado!"),
         ),
       ),
     );
